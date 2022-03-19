ec91d6ef5e6c939b36b5c5e23b940d84f64e23f7
JRFerrerValero <jr@jrferrervalero.es>
2022-03-19 10:41:51 +0100

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("SuperUpdater")>
+<Assembly: AssemblyDescription("Application For Sync Folders")>
+<Assembly: AssemblyCompany("Juan Ramon Ferrer Valero")>
+<Assembly: AssemblyProduct("SuperUpdater")>
+<Assembly: AssemblyCopyright("License GPL")>
+<Assembly: AssemblyTrademark("jr@jrferrervalero.es")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("f4740f6e-9e67-484b-b746-c463d2e88ab1")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SuperUpdater.vb
@@ -0,0 +1,64 @@
+﻿Module SuperUpdater
+
+    ' #######################################
+    ' # Definimos Variables Globales #
+    ' #######################################
+
+    Dim NombreRED As String
+
+
+
+    Sub Main()
+
+        '#################################################################
+        '# Definimos variables basicas para el Funcionamiento de la App  #
+        '#################################################################
+
+
+
+
+
+
+        '######################################################################
+        '# Mostramos la Informacion de la App en el Arranque de la Aplicacion #
+        '######################################################################
+
+        Call MostrarInformacionApp()
+
+        '#####################################
+        '# Carga de Archivo de Configuracion #
+        '#####################################
+
+
+
+
+
+    End Sub
+
+
+    Sub MostrarInformacionApp()
+
+        ' Mostramos Informacion sobre la Aplicacion
+        Console.ForegroundColor = ConsoleColor.Magenta
+        Console.Write(" " & vbCrLf)
+        Console.Write("SuperUpdater v0.10" & vbCrLf)
+        Console.Write("Code By Juan Ramon Ferrer (jr@jrferrervalero.es)" & vbCrLf)
+        Console.Write("-------------------------" & vbCrLf)
+        Console.Write("https://github.com/ElArtista50/" & vbCrLf)
+        Console.Write("License GNU GPLv3" & vbCrLf)
+        Console.Write("-------------------------" & vbCrLf)
+        Console.Write("13/03/2022" & vbCrLf)
+        Console.Write(" " & vbCrLf)
+
+        NombreRED = My.Computer.Name
+        Console.Write("El Nombre de Red de Este Equipo es: " & NombreRED & vbCrLf)
+
+
+
+        Threading.Thread.Sleep(10000)
+
+
+    End Sub
+
+
+End Module
